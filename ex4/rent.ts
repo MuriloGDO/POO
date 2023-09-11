a3f1aff80c9b4d0d8940b784871f68036a75c3df
Murilo Gomes de Oliveira <gdo.murilo@gmail.com>
2023-09-11 16:40:13 -0300

    ex4/

--- ex4/rent.ts
+++ ex4/rent.ts
@@ -1,31 +1,25 @@
 import { User } from "./user";
 import { Bike } from "./bike";
+import { Time } from "./time"
 
 export class Rent{
     public bike: Bike
     public user: User
-    public dateFrom: Date
-    public dateTo: Date
-    public dateReturned?: Date
+    public start: Time
+    public end?: Time
+    public valor?: number
 
-    private constructor(bike: Bike, user: User, dateFrom: Date, dateTo: Date){
+    private constructor(bike: Bike, user: User, start: Time){
         this.bike = bike,
         this.user = user,
-        this.dateFrom = dateFrom,
-        this.dateTo = dateTo
+        this.start = start
     }
 
-    public static create(rents: Rent[], bike: Bike, user: User, startDate: Date, endDate: Date){
-        const canCreate = this.canRent(rents, startDate, endDate)
-        if(canCreate){
-            return new Rent(bike, user, startDate, endDate)
-        }
-        throw new Error('Overlapping dates for this rent.')
+    static create(bike: Bike, user: User,start: Time): Rent {
+        return new Rent(bike, user, start);
     }
 
-    static canRent(rents: Rent[], startDate: Date, endDate: Date): boolean{
-        return !rents.some(rent => {
-            return startDate <= rent.dateTo && endDate >= rent.dateFrom 
-        })
+    valorTot(star: Time, end: Time){
+        return ((end.hour*60 + end.minute) - (star.hour* 60 + star.minute))*0.3
     }
 }
